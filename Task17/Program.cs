64f20e25676f96c1cb3b201376e454180391f923
sergei_perminov <permserg@gmail.com>
2022-10-21 18:48:17 +0300

    Task17 -

--- Task17/Program.cs
+++ Task17/Program.cs
@@ -1,2 +1,57 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 17: Напишите программу, которая принимает на вход координаты точки (X и Y), 
+// причем X <> 0 и Y <> 0 и выдает номер четверти плосости, в которой находится эта точка.
+
+
+
+ Console.Write("\nEnter point coordinates");
+int xCoordinate = GetInputNumber("\nX: ");
+int yCoordinate = GetInputNumber("\nY: ");
+
+Console.Write($"\nCoordinate plane number: {GetQuarterNum(xCoordinate, yCoordinate)}\n");
+
+
+
+// ************* methods section ***************
+int GetInputNumber(string inputText)
+{
+    int num;
+
+    while(true)
+    {
+        Console.Write(inputText);
+        num = Convert.ToInt32(Console.ReadLine());
+        
+        if (IsValid(num)) 
+        {
+            break;
+        }
+    }
+
+    return(num);
+}
+
+bool IsValid(int num)
+{
+    if(num == 0 )
+    {
+        Console.Write("\nError! It's should not be zero!");
+        return false;
+    }
+
+    return true;
+}
+
+int GetQuarterNum(int x, int y)
+{
+    switch (x, y)
+    {
+        case (> 0 , > 0): return 1;
+        case (< 0 , > 0): return 2;
+        case (< 0 , < 0): return 3;
+        case (> 0 , < 0): return 4;
+    }
+
+    return(0);
+}
+
+
