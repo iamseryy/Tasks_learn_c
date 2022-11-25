2cb5b56e69d99bb948c7e9bf646e9186a5c09fe3
desktop <psomail@yandex.ru>
2022-11-25 22:54:34 +0300

    task64-66 ready

---
+++ Task64/Program.cs
@@ -0,0 +1,53 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все
+// натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+
+
+
+int n = GetInputNumber("\nEnter natural number N: ", IsValidNumber);
+
+Console.Write($"\n N = {n} -> ");
+PrintNaturalNumber(n);
+
+
+
+// ************* methods section ***************
+
+void PrintNaturalNumber(int number)
+{
+    if(number == 0) return;
+
+    Console.Write($"{number}{(number > 1 ? ", " : "")}");
+    PrintNaturalNumber(number - 1);
+}
+
+
+int GetInputNumber(string inputText, IsValid isValid)
+{
+    string? data;
+    
+    do
+    {
+        Console.Write(inputText);
+        data = Console.ReadLine();
+    }
+    while(!isValid(data));
+
+    return Convert.ToInt32(data); ; 
+}
+
+bool IsValidNumber(string? data)
+{
+    if (!(int.TryParse(data, out int number) && number > 0))
+    {
+        Console.Write("\nError! It's should be natural number\n");
+        return false;
+    }
+
+    return true;
+}
+
+delegate bool IsValid(string? num);
---
+++ Task66/Program.cs
@@ -0,0 +1,48 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму
+// натуральных элементов в промежутке от M до N. Выполнить с помощью рекурсии.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+
+
+int m = GetInputNumber("\nEnter natural number M: ", IsValidNumber);
+int n = GetInputNumber("\nEnter natural number N: ", IsValidNumber);
+
+Console.Write($"\n M = {m}; N = {n} -> {SumNaturalNumber(m < n ? m : n, m < n ? n : m)}\n");
+
+// ************* methods section ***************
+
+int SumNaturalNumber(int numberFrom, int numberTo)
+{
+    if(numberFrom == numberTo) return numberTo;
+    return numberFrom + SumNaturalNumber(++numberFrom, numberTo);   
+}
+
+
+int GetInputNumber(string inputText, IsValid isValid)
+{
+    string? data;
+    
+    do
+    {
+        Console.Write(inputText);
+        data = Console.ReadLine();
+    }
+    while(!isValid(data));
+
+    return Convert.ToInt32(data); ; 
+}
+
+bool IsValidNumber(string? data)
+{
+    if (!(int.TryParse(data, out int number) && number > 0))
+    {
+        Console.Write("\nError! It's should be natural number\n");
+        return false;
+    }
+
+    return true;
+}
+
+delegate bool IsValid(string? num);
